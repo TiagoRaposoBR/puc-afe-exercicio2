377a6fa6f25ca30441a6ccc4f70ad3b2a7eefafd
Tiago Raposo <tiago.o.raposo@gmail.com>
2020-07-13 21:11:04 -0300

    Testando deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CardsController } from './cards.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CardsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cards.controller.ts
+++
@@ -1,9 +0,0 @@
-import {Controller, Get, Req} from '@nestjs/common';
-import {Request} from 'express';
-@Controller('cards')
-export class CardsController {
-    @Get('/')
-    getIndex(@Req() request: Request): string {
-        return `my first controller: ${request.method}`;
-    }
-}
